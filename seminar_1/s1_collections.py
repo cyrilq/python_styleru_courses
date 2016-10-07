2e2bdc71b03d58d8c75883d6ba3cf9ab3810e3f9
cyrilq <kuharchuk@styleru.net>
2016-10-07 12:39:59 +0400

    Update s1_collections.py

--- seminar_1/s1_collections.py
+++ seminar_1/s1_collections.py
@@ -9,200 +9,124 @@ print("Hello, World", end="!")  # => Hello, World!
 # Так можно получить стоку из консоли
 input_string_var = input("Enter some data: ") # Получаем из консоли строку
 
-# Accessing a previously unassigned variable is an exception.
-# See Control Flow to learn more about exception handling.
-some_unknown_var  # Raises a NameError
+# Условные операторы можно использовать в одну строку:
+"Привет!" if 3 > 2 else "Пока!"  # => "Привет!"
 
-# if can be used as an expression
-# Equivalent of C's '?:' ternary operator
-"yahoo!" if 3 > 2 else 2  # => "yahoo!"
-
-# Lists store sequences
+# Лист хранит значения
 li = []
-# You can start with a prefilled list
+# Можно заполнить его сразу 
 other_li = [4, 5, 6]
 
-# Add stuff to the end of a list with append
+# Добавляем элементы при помощи append
 li.append(1)    # li is now [1]
 li.append(2)    # li is now [1, 2]
-li.append(4)    # li is now [1, 2, 4]
-li.append(3)    # li is now [1, 2, 4, 3]
-# Remove from the end with pop
-li.pop()        # => 3 and li is now [1, 2, 4]
-# Let's put it back
-li.append(3)    # li is now [1, 2, 4, 3] again.
+li.append(3)    # li is now [1, 2, 3]
+li.append(4)    # li is now [1, 2, 3, 4]
+# Убираем последний элемент при помощи pop
+li.pop()        # => 4 and li is now [1, 2, 3]
 
-# Access a list like you would any array
+# К листу можно обращаться как к обычному массиву
 li[0]   # => 1
-# Look at the last element
+# Доступ к последнему элементу
 li[-1]  # => 3
 
-# Looking out of bounds is an IndexError
+# За границы выходить нельзя:
 li[4]  # Raises an IndexError
 
-# You can look at ranges with slice syntax.
-# (It's a closed/open range for you mathy types.)
-li[1:3]   # => [2, 4]
-# Omit the beginning
-li[2:]    # => [4, 3]
-# Omit the end
-li[:3]    # => [1, 2, 4]
-# Select every second entry
+# Можно получать доступ к опр. части листа через срезы (слайсы)
+li[1:3]   # => [2, 3]
+li[2:]    # => [3, 4]
+li[:3]    # => [1, 2, 3]
+# С шагом 2
 li[::2]   # =>[1, 4]
-# Return a reversed copy of the list
+# Реверсим лист
 li[::-1]  # => [3, 4, 2, 1]
-# Use any combination of these to make advanced slices
+# Все работает по шаблону
 # li[start:end:step]
 
-# Make a one layer deep copy using slices
-li2 = li[:]  # => li2 = [1, 2, 4, 3] but (li2 is li) will result in false.
+# Опр. элемент можно удалить при помощи del
+del li[2]  # li is now [1, 2, 4]
 
-# Remove arbitrary elements from a list with "del"
-del li[2]  # li is now [1, 2, 3]
+# remove убирает первый встреченный элемент:
+li.remove(2)  # li is now [1, 4]
+li.remove(2)  # ValueError, потому что 2 нет в листе
 
-# Remove first occurrence of a value
-li.remove(2)  # li is now [1, 3]
-li.remove(2)  # Raises a ValueError as 2 is not in the list
+# Можно вставить опр. элемент в опр. место в листе
+li.insert(1, 2)  # Снова [1, 2, 4] 
 
-# Insert an element at a specific index
-li.insert(1, 2)  # li is now [1, 2, 3] again
-
-# Get the index of the first item found matching the argument
+# Можно получить индекс опр. элемента
 li.index(2)  # => 1
-li.index(4)  # Raises a ValueError as 4 is not in the list
+li.index(3)  # ValueError, потому что 3 нет в листе
 
-# You can add lists
-# Note: values for li and for other_li are not modified.
+# Листы можно складывать, при таком сложении сами листы-слагаемые не меняются!
 li + other_li  # => [1, 2, 3, 4, 5, 6]
 
-# Concatenate lists with "extend()"
+# А так меняются
 li.extend(other_li)  # Now li is [1, 2, 3, 4, 5, 6]
 
-# Check for existence in a list with "in"
+# Можно проверить наличие элемента в листе при помощи in
 1 in li  # => True
 
-# Examine the length with "len()"
-len(li)  # => 6
-
-
-# Tuples are like lists but are immutable.
+# Тьюпл (или кортеж) — это неизменяемый лист
 tup = (1, 2, 3)
 tup[0]      # => 1
 tup[0] = 3  # Raises a TypeError
-
-# Note that a tuple of length one has to have a comma after the last element but
-# tuples of other lengths, even zero, do not.
 type((1))   # => <class 'int'>
 type((1,))  # => <class 'tuple'>
 type(())    # => <class 'tuple'>
-
-# You can do most of the list operations on tuples too
 len(tup)         # => 3
 tup + (4, 5, 6)  # => (1, 2, 3, 4, 5, 6)
 tup[:2]          # => (1, 2)
 2 in tup         # => True
-
-# You can unpack tuples (or lists) into variables
-a, b, c = (1, 2, 3)  # a is now 1, b is now 2 and c is now 3
-# You can also do extended unpacking
-a, *b, c = (1, 2, 3, 4)  # a is now 1, b is now [2, 3] and c is now 4
-# Tuples are created by default if you leave out the parentheses
+# Тьюплы можно использовать даже без скобок 
+a, b, c = (1, 2, 3)  
+a, *b, c = (1, 2, 3, 4)  
 d, e, f = 4, 5, 6
-# Now look how easy it is to swap two values
-e, d = d, e  # d is now 5 and e is now 4
+e, d = d, e 
 
 
-# Dictionaries store mappings
+# Словари ведут себя обычно
 empty_dict = {}
-# Here is a prefilled dictionary
 filled_dict = {"one": 1, "two": 2, "three": 3}
 
-# Note keys for dictionaries have to be immutable types. This is to ensure that
-# the key can be converted to a constant hash value for quick look-ups.
-# Immutable types include ints, floats, strings, tuples.
-invalid_dict = {[1,2,3]: "123"}  # => Raises a TypeError: unhashable type: 'list'
-valid_dict = {(1,2,3):[1,2,3]}   # Values can be of any type, however.
+# Ключи словарей обязаны принадлежать неизменяемому типу! 
+invalid_dict = {[1,2,3]: "123"}  # => TypeError: unhashable type: 'list'
+valid_dict = {(1,2,3):[1,2,3]}   # Values могут быть любого типа
 
-# Look up values with []
+# Искать значения можно квадратными скобками
 filled_dict["one"]  # => 1
 
-# Get all keys as an iterable with "keys()". We need to wrap the call in list()
-# to turn it into a list. We'll talk about those later.  Note - Dictionary key
-# ordering is not guaranteed. Your results might not match this exactly.
-list(filled_dict.keys())  # => ["three", "two", "one"]
-
-
-# Get all values as an iterable with "values()". Once again we need to wrap it
-# in list() to get it out of the iterable. Note - Same as above regarding key
-# ordering.
-list(filled_dict.values())  # => [3, 2, 1]
-
-
-# Check for existence of keys in a dictionary with "in"
+# Проверяем наличие ключа при помощи "in"
 "one" in filled_dict  # => True
 1 in filled_dict      # => False
-
-# Looking up a non-existing key is a KeyError
 filled_dict["four"]  # KeyError
 
-# Use "get()" method to avoid the KeyError
+# Используем метод "get()" во избежание ошибки 
 filled_dict.get("one")      # => 1
 filled_dict.get("four")     # => None
-# The get method supports a default argument when the value is missing
+# Можно установить аргумент по умолчанию
 filled_dict.get("one", 4)   # => 1
 filled_dict.get("four", 4)  # => 4
 
-# "setdefault()" inserts into a dictionary only if the given key isn't present
-filled_dict.setdefault("five", 5)  # filled_dict["five"] is set to 5
-filled_dict.setdefault("five", 6)  # filled_dict["five"] is still 5
-
-# Adding to a dictionary
+# Добавление в словарь
 filled_dict.update({"four":4})  # => {"one": 1, "two": 2, "three": 3, "four": 4}
 #filled_dict["four"] = 4        #another way to add to dict
 
-# Remove keys from a dictionary with del
+# Удаление ключа
 del filled_dict["one"]  # Removes the key "one" from filled dict
 
-# From Python 3.5 you can also use the additional unpacking options
-{'a': 1, **{'b': 2}}  # => {'a': 1, 'b': 2}
-{'a': 1, **{'a': 2}}  # => {'a': 2}
-
-
-
-# Sets store ... well sets
+# Сеты (множества) — из названия понятно, что это (привет, дискра!)
 empty_set = set()
-# Initialize a set with a bunch of values. Yeah, it looks a bit like a dict. Sorry.
 some_set = {1, 1, 2, 2, 3, 4}  # some_set is now {1, 2, 3, 4}
-
-# Similar to keys of a dictionary, elements of a set have to be immutable.
-invalid_set = {[1], 1}  # => Raises a TypeError: unhashable type: 'list'
+invalid_set = {[1], 1}  # => TypeError: unhashable type: 'list'
 valid_set = {(1,), 1}
-
-# Can set new variables to a set
-filled_set = some_set
-
-# Add one more item to the set
-filled_set.add(5)  # filled_set is now {1, 2, 3, 4, 5}
-
-# Do set intersection with &
+filled_set.add(5) 
 other_set = {3, 4, 5, 6}
 filled_set & other_set  # => {3, 4, 5}
-
-# Do set union with |
 filled_set | other_set  # => {1, 2, 3, 4, 5, 6}
-
-# Do set difference with -
 {1, 2, 3, 4} - {2, 3, 5}  # => {1, 4}
-
-# Do set symmetric difference with ^
 {1, 2, 3, 4} ^ {2, 3, 5}  # => {1, 4, 5}
-
-# Check if set on the left is a superset of set on the right
 {1, 2} >= {1, 2, 3} # => False
-
-# Check if set on the left is a subset of set on the right
 {1, 2} <= {1, 2, 3} # => True
-
-# Check for existence in a set with in
 2 in filled_set   # => True
 10 in filled_set  # => False
